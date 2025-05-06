722268b960b978a325f7f20e1b29280849b10197
Walison Magalhães <wcm-@live.com>
2025-05-06 01:10:05 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,59 +2,56 @@
 
 if ! command -v zsh &> /dev/null; then
     echo "Zsh not found. Installing..."
-        sudo pacman -S --needed --noconfirm zsh
-        fi
+    sudo pacman -S --needed --noconfirm zsh
+fi
 
-        echo "Changing the default shell to zsh..."
-        chsh -s $(which zsh) "$USER"
-        echo "The default shell has been changed. Opening a new terminal window."
+echo "Changing the default shell to zsh..."
+chsh -s $(which zsh) "$USER"
+echo "The default shell has been changed. Opening a new terminal window."
 
-        echo "Updating the system..."
-        sudo pacman -Syu --noconfirm
+echo "Updating the system..."
+sudo pacman -Syu --noconfirm
 
-        echo "Installing Zen Browser"
-        yay -S --needed --noconfirm zen-browser-bin
+echo "Installing Zen Browser"
+yay -S --needed --noconfirm zen-browser-bin
 
-        echo "Installing CUPS and Epson L3150 drivers via yay..."
-        yay -S --needed --noconfirm cups epson-inkjet-printer-escpr
+echo "Installing CUPS and Epson L3150 drivers via yay..."
+yay -S --needed --noconfirm cups epson-inkjet-printer-escpr
 
-        echo "Enabling and starting the CUPS service..."
-        sudo systemctl enable --now cups.service
+echo "Enabling and starting the CUPS service..."
+sudo systemctl enable --now cups.service
 
-        echo "Installing NVM"
-        yay -S --needed --noconfirm nvm
+echo "Installing NVM"
+yay -S --needed --noconfirm nvm
 
-        echo "Installing oh-my-zsh"
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-        echo "Installing zsh-syntax-highlighting"
-        git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+echo "Installing zsh-syntax-highlighting"
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
-        echo "Installing zsh-autosuggestions"
-        git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+echo "Installing zsh-autosuggestions"
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
-        echo "Installing the powerlevel10k theme..."
-        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+echo "Installing the powerlevel10k theme..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-        # Installs Powerlevel10k suggested fonts
-        echo "Installing Powerlevel10k suggested fonts..."
-        yay -S --needed --noconfirm \
-            adobe-source-code-pro-fonts \
-                nerd-fonts-fira-code \
-                    nerd-fonts-hack \
-                        nerd-fonts-jetbrains-mono \
-                            nerd-fonts-meslo-lg \
-                                nerd-fonts-noto-sans-mono
+# Installs Powerlevel10k suggested fonts
+echo "Installing Powerlevel10k suggested fonts..."
+yay -S --needed --noconfirm \
+    adobe-source-code-pro-fonts \
+    nerd-fonts-fira-code \
+    nerd-fonts-hack \
+    nerd-fonts-jetbrains-mono \
+    nerd-fonts-meslo-lg \
+    nerd-fonts-noto-sans-mono
 
-                                # Activates the powerlevel10k theme in the zsh configuration file
-                                echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+# Activates the powerlevel10k theme in the zsh configuration file
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
 
-                                # Activates the zsh-syntax-highlighting plugin in the zsh configuration file
-                                echo 'plugins+=(zsh-syntax-highlighting)' >> ~/.zshrc
+# Activates the zsh-syntax-highlighting plugin in the zsh configuration file
+echo 'plugins+=(zsh-syntax-highlighting)' >> ~/.zshrc
 
-                                # Installs Neovim and fzf
-                                echo "Installing Neovim and fzf..."
-                                yay -S --needed --noconfirm neovim fzf
-
-
-                                
+# Installs Neovim and fzf
+echo "Installing Neovim and fzf..."
+yay -S --needed --noconfirm neovim fzf
