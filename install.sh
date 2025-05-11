a7f297678dd82f3e7513c6cf7c74d7b14a15465b
Walison Magalhães <wcm-@live.com>
2025-05-11 14:59:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-echo "Updating the system..."
-sudo pacman -Syu --noconfirm
-sudo pacman -S --needed --noconfirm git base-devel
-
-echo "Installing yay..."
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd ..
-
 if ! command -v zsh &> /dev/null; then
     echo "Zsh not found. Installing..."
     sudo pacman -S --needed --noconfirm zsh
@@ -20,7 +10,7 @@ chsh -s $(which zsh) "$USER"
 echo "The default shell has been changed. Opening a new terminal window."
 
 echo "Updating the system..."
-sudo pacman -Syu --noconfirm
+yay -Syu --noconfirm
 
 echo "Installing Zen Browser"
 yay -S --needed --noconfirm zen-browser-bin
@@ -43,7 +33,6 @@ git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_
 echo "Installing the powerlevel10k theme..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-# Installs Powerlevel10k suggested fonts
 echo "Installing Powerlevel10k suggested fonts..."
 yay -S --needed --noconfirm \
     adobe-source-code-pro-fonts \
@@ -53,12 +42,17 @@ yay -S --needed --noconfirm \
     nerd-fonts-meslo-lg \
     nerd-fonts-noto-sans-mono
 
-# Activates the powerlevel10k theme in the zsh configuration file
+echo "Activates the powerlevel10k theme in the zsh configuration file..."
 echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
 
-# Activates the zsh-syntax-highlighting plugin in the zsh configuration file
-echo 'plugins+=(zsh-syntax-highlighting)' >> ~/.zshrc
+echo "Activates the zsh-syntax-highlighting plugin in the zsh configuration file..."
+echo 'plugins+=(zsh-syntax-autosuggestions)' >> ~/.zshrc
+echo 'source $ZSH/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh' >> ~/.zshrc
 
-# Installs Neovim and fzf
+echo "Activates the zsh-syntax-highlighting plugin in the zsh configuration file..."
+echo 'plugins+=(zsh-syntax-highlighting)' >> ~/.zshrc
+echo 'source $ZSH/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
+
 echo "Installing Neovim and fzf..."
 yay -S --needed --noconfirm neovim fzf
+echo '[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh' >> ~/.zshrc
