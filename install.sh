e0aff5ba289c502b0913d5175473f9df03b2416e
Walison Magalhães <wcm-@live.com>
2025-05-20 15:40:02 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,10 +54,8 @@ yay -S --needed --noconfirm insomnia-bin
 echo 'Installing Discord...'
 yay -S --needed --noconfirm discord
 
-echo 'Installing Docker...'
+echo 'Installing Docker Desktop...'
 yay -S --needed --noconfirm docker-desktop
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
 
 echo 'Installing CUPS and Epson L3150 drivers via yay...'
 yay -S --needed --noconfirm cups epson-inkjet-printer-escpr
